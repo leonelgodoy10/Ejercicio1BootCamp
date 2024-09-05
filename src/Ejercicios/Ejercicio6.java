5673055ed2b21bbed91b165b284d61673f22fdca
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-05 15:00:24 -0400

    Se sube el ejercicio6

---
+++ src/Ejercicios/Ejercicio6.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        double a = 4;
+        double b = 9;
+        double c = 13;
+        double prom = (a+b+c)/3;
+
+        System.out.println("El promedio de los 3 numeros ingresados es : "+prom);
+    }
+}
