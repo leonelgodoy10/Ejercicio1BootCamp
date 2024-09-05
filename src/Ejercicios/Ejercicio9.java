fac1786c49884c2ec2893123b1914d00a3d2d97a
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-05 15:22:07 -0400

    se modifica nombre de la variable del ejercicio9

--- src/Ejercicios/Ejercicio9.java
+++ src/Ejercicios/Ejercicio9.java
@@ -2,10 +2,10 @@ package Ejercicios;
 
 public class Ejercicio9 {
     public static void main(String[] args) {
-        int nf = 50;
+        int N = 50;
         int suma = 0;
         for (int i = 1; i < 2; i++) {
-            suma = (nf*(nf+1))/2;
+            suma = (N*(N+1))/2;
         }
         System.out.println(suma);
     }
