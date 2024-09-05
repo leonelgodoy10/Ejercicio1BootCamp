c312b76896764a2fb41e54b209985d971dfdfcf3
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-05 15:05:51 -0400

    Se sube el ejercicio7

---
+++ src/Ejercicios/Ejercicio7.java
@@ -0,0 +1,9 @@
+package Ejercicios;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        double b = 5 , h = 8;
+        double area = 0.5*b*h;
+        System.out.println("El area es de : "+area);
+    }
+}
