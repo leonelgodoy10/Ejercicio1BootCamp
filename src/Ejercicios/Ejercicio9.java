522ae2b5451cd57051b6872240da9e387f825d13
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-05 15:19:02 -0400

    Se sube el ejercicio9

---
+++ src/Ejercicios/Ejercicio9.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        int nf = 50;
+        int suma = 0;
+        for (int i = 1; i < 2; i++) {
+            suma = (nf*(nf+1))/2;
+        }
+        System.out.println(suma);
+    }
+}
