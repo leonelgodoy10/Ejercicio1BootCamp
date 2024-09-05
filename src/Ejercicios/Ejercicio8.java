4819e9cefc46738c594f2143f40bca9f6fdffb67
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-05 15:10:58 -0400

    Se sube el ejercicio8

---
+++ src/Ejercicios/Ejercicio8.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        int c = 25;
+        double f = 1.8*c+32;
+
+        System.out.println(c+" grados Celsius son "+f+" grados Fahrenheit");
+    }
+}
