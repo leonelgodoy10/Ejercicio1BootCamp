0321ec518a67df5753ca95ccc478749b49b92d04
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-05 17:06:47 -0400

    Se sube el ejercicio15

---
+++ src/Ejercicios/Ejercicio15.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        double r = 7;
+
+        double area = Math.PI*r*r;
+
+        System.out.println("EL area del circulo es : "+area);
+    }
+}
