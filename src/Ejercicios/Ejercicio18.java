f5ae58ae5eeba5059d4c34a3bbc086dddd97d10a
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-09 09:47:39 -0300

    Se sube el ejercicio18

---
+++ src/Ejercicios/Ejercicio18.java
@@ -0,0 +1,9 @@
+package Ejercicios;
+
+public class Ejercicio18 {
+    public static void main(String[] args) {
+       int a = 2, b = 8;
+        System.out.println("La potencia de "+a+" elevado a "+b+" es: "+Math.pow(2,8));
+
+    }
+}
