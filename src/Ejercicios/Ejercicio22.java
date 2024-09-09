61947c0d7f0f770953e09d88c224e45125806f87
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-09 10:01:07 -0300

    Se sube el ejercicio22

---
+++ src/Ejercicios/Ejercicio22.java
@@ -0,0 +1,9 @@
+package Ejercicios;
+
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        int a = 987654;
+        String con = String.valueOf(a);
+        System.out.println("La cantidad de cifras del numero "+a+" es de: "+con.length());
+    }
+}
