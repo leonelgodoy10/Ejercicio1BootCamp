b007b5413b439c7f63b52bde472e18e3d3255145
Leonel Godoy <leonel.godoy@tsoftglobal.com>
2024-09-09 10:06:13 -0300

    Se sube el ejercicio25

---
+++ src/Ejercicios/Ejercicio25.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        int a = 7;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i+" X "+a+" = "+i*a);
+        }
+    }
+}
